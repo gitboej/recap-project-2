e6b2225644d35d0ed8dd6b06cf30fcb10fce281c
Lars Bonus <lars.bonus@outlook.com>
2023-03-16 11:04:11 +0100

    add js functionaltiy

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// phew… not a lot going on here. Please add some code!
+console.clear();
+
+const bookmarkButton = document.querySelector('[data-js="bookmark-button"]');
+const answerButton = document.querySelector('[data-js="answer-button"]');
+const answer = document.querySelector('[data-js="answer"]');
+
+bookmarkButton.addEventListener("click", () => {
+  bookmarkButton.classList.toggle("bookmark--active");
+});
+
+answerButton.addEventListener("click", () => {
+  swapAnswer();
+  answer.classList.toggle("card__answer--active");
+});
+
+function swapAnswer() {
+  if (answerButton.innerHTML === "Hide answer") {
+    answerButton.innerHTML = "Show answer";
+  } else {
+    answerButton.innerHTML = "Hide answer";
+  }
+}
